7ffbd9001ee3a8bae2431e94a6571a78fc7a3b3c
Bhavyaxworkz <sdbhavya8@gmail.com>
2023-07-12 14:17:41 +0530

    adding ChaiPoint1 files crud

---
+++ ChaiPoint1Tester.java
@@ -0,0 +1,12 @@
+class ChaiPoint1Tester{
+public static void main(String addchai[]){
+	ChaiPoint1.addChai("Masala Tea");
+	ChaiPoint1.addChai("Ginger Tea");
+	ChaiPoint1.addChai("Lemon Tea");
+	ChaiPoint1.addChai("Elaichai Tea");
+	//get all the tea at once
+ChaiPoint1.getAllChaiAvailable();
+}
+
+
+}
